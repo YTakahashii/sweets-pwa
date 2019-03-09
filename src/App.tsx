07d28b355100e54f81f2b173fa30e257036c6a01
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-03-10 03:30:19 +0900

    :lipstick: primary colorを#3498DB（水色）に設定
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import './App.css';
 import MyBottomNavigation from './components/BottomNavigation/BottomNavigation';
-
+import Navigationbar from './components/Navigationbar/Navigationbar';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { theme } from './utils/theme';
 class App extends React.Component {
   public render() {
     return (
-      <div className='App'>
-        <MyBottomNavigation />
-      </div>
+      <MuiThemeProvider theme={theme} >
+        <div className='App'>
+          <Navigationbar />
+          <MyBottomNavigation />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
