9eb4246fe33c5b75c51edf4deaebe909034e4e07
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-03-10 04:20:02 +0900

    :+1: iOSのスクロール無効化に対応
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- src/index.tsx
+++ src/index.tsx
@@ -8,4 +8,31 @@ ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
+
+window.addEventListener('touchmove', event => {
+  event.preventDefault();
+});
+
+/*
+var textarea = document.getElementById('textarea');
+textarea.scrollTop = 1;
+
+window.addEventListener('touchmove', function(event) {
+  if (event.target === textarea && textarea.scrollTop !== 0 && textarea.scrollTop + textarea.clientHeight !== textarea.scrollHeight) {
+    event.stopPropagation();
+  }
+  else {
+    event.preventDefault();
+  }
+});
+
+textarea.addEventListener('scroll', function(event) {
+  if (textarea.scrollTop === 0) {
+    textarea.scrollTop = 1;
+  }
+  else if (textarea.scrollTop + textarea.clientHeight === textarea.scrollHeight) {
+    textarea.scrollTop = textarea.scrollTop - 1;
+  }
+});
+*/
 registerServiceWorker();
