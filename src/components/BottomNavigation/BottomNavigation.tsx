d1acbc93606a6c205110e05be9aa8aa4d00fd5b7
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-03-10 01:03:19 +0900

    :sparkles: BottomNavigation実装
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

---
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import CakeIcon from '@material-ui/icons/Cake';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const styles = () => createStyles({
+    root: {
+        marginTop: 'auto',
+        display: 'flex',
+        justifyContent: 'space-around',
+        width: '100%'
+    },
+});
+
+type MyBottomNavigationProps = WithStyles<typeof styles>;
+interface MyBottomNavigationState {
+    value: number;
+}
+
+class MyBottomNavigation extends React.Component<MyBottomNavigationProps, MyBottomNavigationState>  {
+    state = {
+        value: 0,
+    };
+
+    handleChange = (event: React.ChangeEvent, value: number) => {
+        this.setState({ value });
+        // console.log(event.currentTarget.textContent); // labelの値
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { value } = this.state;
+
+        return (
+            <BottomNavigation
+                value={value}
+                onChange={this.handleChange}
+                showLabels
+                className={classes.root}
+            >
+                <BottomNavigationAction label="スイーツ" icon={<CakeIcon />} />
+                <BottomNavigationAction label="地図" icon={<LocationOnIcon />} />
+                <BottomNavigationAction label="お気に入り" icon={<FavoriteIcon />} />
+            </BottomNavigation>
+        );
+    }
+}
+
+export default withStyles(styles)(MyBottomNavigation);
