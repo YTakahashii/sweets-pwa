5db4e16746f8a601a98c920026032b891774ee3c
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-03-10 05:42:51 +0900

    :construction: テスト用のコンテンツ領域
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import MyBottomNavigation from './components/BottomNavigation/BottomNavigation';
 import Navigationbar from './components/Navigationbar/Navigationbar';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { theme } from './utils/theme';
+import Content from './components/Content/Content';
+// import SweetsCardList from './components/SweetsCardList/SweetsCardList';
 class App extends React.Component {
   public render() {
     return (
       <MuiThemeProvider theme={theme} >
         <div className='App'>
           <Navigationbar />
+          <Content />
           <MyBottomNavigation />
         </div>
       </MuiThemeProvider>
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { withStyles, WithStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const styles = (theme: Theme) => createStyles({
+    root: {
+        width: '100%',
+        flexGrow: 1,
+        maxHeight: 650,
+        overflow: 'scroll'
+    },
+});
+
+type ContentProps = WithStyles<typeof styles>;
+
+const Content = (props: ContentProps) => {
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+        </div>
+    );
+}
+
+export default withStyles(styles)(Content);
