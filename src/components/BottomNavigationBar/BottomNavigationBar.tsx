75ce3b678f0383087136e926a0ef22e7587e998b
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2019-09-29 18:34:51 +0900

    test: 強制アップデート６

--- src/components/BottomNavigationBar/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar/BottomNavigationBar.tsx
@@ -47,7 +47,7 @@ export const BottomNavigationBar: React.FC = () => {
         value='/sweets'
       />
       <BottomNavigationAction
-        label='地図 T5'
+        label='地図 T6'
         icon={<LocationOnIcon />}
         value='/maps'
       />
