ac3cad74cb47d3827dd7c70fb174e09695c0d098
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2019-09-29 16:28:18 +0900

    fix: 強制アップデート

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 function onUpdateHandler(registration: ServiceWorkerRegistration) {
   // Make sure that any new version of a service worker will take over the page
   // and become activated immediately.
-  const waitingServiceWorker = registration.waiting;
-  if (waitingServiceWorker) {
-    waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
-  }
-
-  const link = document.createElement('a');
-  link.classList.add('update-notification');
-  link.setAttribute('href', '#');
-  link.innerHTML = 'アップデートが利用可能です。🎉 クリックしてインストール。';
-
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    window.location.reload();
-  });
-
-  document.querySelector('body')!.appendChild(link);
+  window.location.reload();
 }
 
 serviceWorker.register({ onUpdate: onUpdateHandler });
