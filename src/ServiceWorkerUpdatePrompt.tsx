4f4ce1d1468cce3e273739a2e70040f656f24d38
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-01-17 00:51:51 +0900

    fix: ダイアログの外を押しても閉じないように

--- src/ServiceWorkerUpdatePrompt.tsx
+++ src/ServiceWorkerUpdatePrompt.tsx
@@ -1,29 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { IonAlert } from '@ionic/react';
 
-export const ServiceWorkerUpdatePrompt: React.FC<{ registration: ServiceWorkerRegistration }> = ({ registration }) => {
-  const [isOpen, setOpen] = useState(!!registration.waiting);
-
-  const handleDismiss = () => {
-    setOpen(false);
-  };
-
-  return (
-    <IonAlert
-      isOpen={isOpen}
-      onDidDismiss={handleDismiss}
-      header='ニューリリース'
-      message='新しいバージョンがリリースされました。'
-      buttons={[
-        {
-          text: 'アップデート',
-          handler: () => {
-            registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
-            handleDismiss();
-            window.location.reload();
-          },
+export const ServiceWorkerUpdatePrompt: React.FC<{ registration: ServiceWorkerRegistration }> = ({ registration }) => (
+  <IonAlert
+    isOpen={!!registration.waiting}
+    header='ニューリリース'
+    message='新しいバージョンがリリースされました。'
+    buttons={[
+      {
+        text: 'アップデート',
+        handler: () => {
+          registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
+          window.location.reload();
         },
-      ]}
-    />
-  );
-};
+      },
+    ]}
+  />
+);
